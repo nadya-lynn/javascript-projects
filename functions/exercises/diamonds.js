a6f48d84f2faca0cca2407a95483dc321ba6f62e
nadya-lynn <154042485+nadya-lynn@users.noreply.github.com>
2024-01-25 16:07:44 -0600

    Checking in functions exercises

---
+++ functions/exercises/diamonds.js
@@ -0,0 +1,24 @@
+function makeLine(size) {
+    let line = '';
+    for (let i = 0; i < size; i++) {
+      line += '#';
+    }
+    return line;
+  }
+  
+
+  function makeRectangle(width, height) {
+    let rectangle = '';
+    for (let i = 0; i < height; i++) {
+      rectangle += (makeLine(width) + '\n');
+    }
+    return rectangle.slice(0, -1);
+  }
+
+  function makeDownwardStairs(height) {
+    let stairs = '';
+    for (let i = 0; i < height; i++) {
+      stairs += (makeLine(i+1) + '\n');
+    }
+    return stairs.slice(0, -1);
+  }
