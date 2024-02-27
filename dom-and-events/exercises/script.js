7a9dc640d11f274ece91c27c9bd47092edb45c96
nadya-lynn <154042485+nadya-lynn@users.noreply.github.com>
2024-02-26 19:19:52 -0600

    Checking in DOM exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,22 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+
+     missionAbort.addEventListener("mouseover", function(){
+        missionAbort.style.background = "red";
+     });
+
+     missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });
+
+     missionAbort.addEventListener("click", function(){
+        let abortQuestion = window.confirm("Are you sure you want to abort the mission?");
+        if (abortQuestion){paragraph.innerHTML = "Mission aborted! Space shuttle returning home."}
+     });
 }
 
 window.addEventListener("load", init);
